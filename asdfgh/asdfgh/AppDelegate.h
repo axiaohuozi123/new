52cdbf8f7583ff13710a7fcf95076da31791ae4d
xiao123 <34567@qq.com>
2017-10-30 21:21:32 +0800

    sdfgh

---
+++ asdfgh/asdfgh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  asdfgh
+//
+//  Created by xiaolongqing on 2017/10/30.
+//  Copyright © 2017年 dfg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ asdfgh/asdfgh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  asdfgh
+//
+//  Created by xiaolongqing on 2017/10/30.
+//  Copyright © 2017年 dfg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
